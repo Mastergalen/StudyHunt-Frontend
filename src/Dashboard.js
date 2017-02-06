7706011ec5173dd4f3fc299989a2b7f245a13183
Andrei Boscor <andyboscor@yahoo.com>
2017-02-06 13:24:11 +0000

    Added sticky nav bar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,11 +5,19 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import Drawer from 'material-ui/Drawer';
 import MenuItem from 'material-ui/MenuItem';
 import RaisedButton from 'material-ui/RaisedButton';
+import CardExample from './CardExample.js';
+import { StickyContainer, Sticky } from 'react-sticky';
+
 injectTapEventPlugin();
 
 var buttonStyles = {
   margin: '20px'
 }
+var stickyNavStyle = {
+  zIndex: '999'
+}
+var cards = [];
+
 class Dashboard extends Component {
   state = {
     title: "StudyHunter",
@@ -18,14 +26,23 @@ class Dashboard extends Component {
 
   handleToggle = () => this.setState({open: !this.state.open});
 
+  expandedCards(){
+
+    for(var i=0;i<=20;i++)
+    cards.push(<CardExample />);
+  }
   render() {
     return (
       <div>
-
+      <StickyContainer>
+        <Sticky style={stickyNavStyle}>
         <AppBar
         title={this.state.title}
         iconClassNameRight="muidocs-icon-navigation-expand-more"
+        onLeftIconButtonTouchTap={this.handleToggle}
+
         />
+          </Sticky>
         <Drawer
           docked={false}
           width={200}
@@ -35,10 +52,15 @@ class Dashboard extends Component {
           <MenuItem onTouchTap={this.handleClose}>Menu Item</MenuItem>
           <MenuItem onTouchTap={this.handleClose}>Menu Item 2</MenuItem>
         </Drawer>
+
         <div >
         <RaisedButton style={buttonStyles} primary={true} label="Browse A-Z" />
         <RaisedButton style={buttonStyles} primary={true} label="Search" />
         </div>
+
+        {this.expandedCards()}
+        {cards}
+        </StickyContainer>
       </div>
     );
   }
