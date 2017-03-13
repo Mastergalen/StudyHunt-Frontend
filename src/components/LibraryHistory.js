93f5990867f2a4a2eb0fc23a9830db96a33deebd
Mastergalen <Mastergalen@users.noreply.github.com>
2017-03-13 13:20:03 +0000

    Add library history

---
+++ src/components/LibraryHistory.js
@@ -0,0 +1,85 @@
+import React, { Component } from 'react';
+import {makeWidthFlexible, XYPlot, XAxis, YAxis, Hint, HorizontalGridLines, LineMarkSeries} from 'react-vis';
+import env from '../constants/env';
+
+import "react-vis/dist/style.css";
+
+const FlexibleXYPlot = makeWidthFlexible(XYPlot);
+
+class LibraryHistory extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      data: null,
+      hint: null,
+      yMax: null,
+    }
+  }
+
+  async componentWillMount() {
+    let res = await fetch(`${env.BACKEND_URL}/api/v1/libraries/${this.props.params.libraryId}/history`)
+
+    let json = await res.json();
+
+    let yMax = 0;
+    for(let i = 0; i < json.history.length; i++) {
+      json.history[i].x = Date.parse(json.history[i]['time']);
+      json.history[i].y = json.history[i]['vacantSeats'];
+
+      if(json.history[i].y > yMax) {
+        yMax = json.history[i].y;
+      }
+
+      delete json.history[i].time;
+      delete json.history[i].vacantSeats;
+    }
+
+    this.setState({
+      data: json.history,
+      yMax: yMax * 1.2
+    });
+  }
+
+  _rememberHint(hint) {
+    this.setState({hint});
+  }
+
+  _forgetHint() {
+    this.setState({
+      hint: null
+    });
+  }
+
+  render() {
+    const {hint} = this.state;
+    return (
+      <div>
+        <h2>Free Seats Last 24 hours</h2>
+        <FlexibleXYPlot
+          height={600}
+          xType="time-utc"
+          yType="linear"
+          yDomain={[0, this.state.yMax]}
+        >
+          <HorizontalGridLines tickTotal={this.state.yMax} />
+          <LineMarkSeries
+            data={this.state.data}
+            size={5}
+            onValueMouseOver={this._rememberHint.bind(this)}
+            onValueMouseOut={this._forgetHint.bind(this)}
+          />
+          <XAxis title="Time" />
+          <YAxis title="Free seats" tickTotal={this.state.yMax} />
+          {hint ?
+            <Hint value={hint}/> :
+            null
+          }
+        </FlexibleXYPlot>
+      </div>
+    );
+  }
+}
+
+export default LibraryHistory;
--- src/routes.js
+++ src/routes.js
@@ -7,14 +7,17 @@ import App from './components/App';
 import Dashboard from './components/Dashboard';
 import NoMatch from "./components/NoMatch";
 import LibrarySimulator from './components/simulation/LibrarySimulator';
-import LibraryPage from './components/LibraryPage.js';
+import LibraryPage from './components/LibraryPage';
+import LibraryHistory from './components/LibraryHistory';
+
 const Routes = (
   <Route path="/" component={App}>
     <IndexRoute component={Dashboard}/>
     <Route path="browse" component={BrowseAZ}/>
     <Route path="search" component={SearchPage}/>
     <Route path="simulator" component={LibrarySimulator} />
-    <Route path="library/:libraryid" component={LibraryPage} />
+    <Route path="library/:libraryId/history" component={LibraryHistory} />
+    <Route path="library/:libraryId" component={LibraryPage} />
     <Route path="*" component={NoMatch}/>
   </Route>
 );
